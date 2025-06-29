2cebe4543c4a3b018b31f1fcf8415f6fb137d01f
Gijo John <gijojohn.7904@gmail.com>
2025-06-29 23:28:43 +0530

    Update zonepulse_app.py

--- zonepulse_app.py
+++ zonepulse_app.py
@@ -317,7 +317,7 @@ components.html("""
     box-shadow: 0 2px 4px rgba(0,0,0,0.05);
 '>
     Built by <b>Gijo Kochuparambil John</b> –  Assistant Manager, Sourcing & Onboarding, Swiggy<br>
-    📧 <a href='mailto:gijo.j@swiggy.in' style='text-decoration:none;color:#0072b1;'>gijo.j@swiggy.in</a> |
+     <a href='mailto:gijo.j@swiggy.in' style='text-decoration:none;color:#0072b1;'>gijo.j@swiggy.in</a> |
      <a href='https://www.linkedin.com/in/gijojohn/' target='_blank' style='text-decoration:none;color:#0072b1;'>LinkedIn</a><br>
     <sub style='color:#666;'>#FleetFirst | Empowering Swiggy with data-driven fleet optimization</sub>
 </div>
