cb9ea80908b5a1567b93a0048a32e31ce1b6868c
Gijo John <gijojohn.7904@gmail.com>
2025-07-01 02:10:31 +0530

    Update zonepulse_app.py

--- zonepulse_app.py
+++ zonepulse_app.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 import streamlit as st
 import pandas as pd
 import numpy as np
-import altair as alt
+import altair as alt 
 
 def check_password():
     def password_entered():
